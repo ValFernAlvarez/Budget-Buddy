a5a4c6d681000159968512c63bb4053bc381d54d
ValFernAlvarez <valeria.fernalva@gmail.com>
2025-11-27 16:26:21 -0400

    Primeros comentario.

--- budget_buddy.cpp
+++ budget_buddy.cpp
@@ -1,1 +1,21 @@
-// hola aqui trabajaremos
+// ========== Budget Buddy: A Personal Finance Tracker ==========
+
+// ----------------------- LIBRERÍAS ----------------------
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <limits>
+#include <algorithm>
+
+// ----------------------- NAMESPACE ----------------------
+using namespace std;
+
+// ----------------------- ESTRUCTURAS ----------------------
+struct Gasto {
+    double monto;
+    string categoria;
+    string fecha;
+    string descripcion;
+};
